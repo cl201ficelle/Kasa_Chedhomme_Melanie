41d61e7c8c42747f7669ea1aad2b0f652ea0ce08
Melanie C <melanie.chedhomme96@gmail.com>
2023-11-02 17:41:20 +0100

    supp ligne inutile

--- my-app/src/pages/Logements.js
+++ my-app/src/pages/Logements.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react"
-import { Link } from "react-router-dom";
 import "../styles/page/Logement.css"
 import { useParams } from "react-router-dom";
 import Error from "./Error";
