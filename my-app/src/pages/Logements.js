774bf110f748cd901f25e930e9cba726a519b7ef
Melanie C <melanie.chedhomme96@gmail.com>
2023-11-07 15:22:42 +0100

    séparation liste équipements

--- my-app/src/pages/Logements.js
+++ my-app/src/pages/Logements.js
@@ -57,7 +57,12 @@ const Location = () => {
                         <div>
               <div className="collapse_logement">
               <div className="description">< Collapsible label="Description" description={logement.description}/></div>
-              <div className="equipement">< Collapsible label="Équipements" description={logement.equipments}/></div>
+              <Collapsible
+          label="Équipements"
+          description={logement.equipments.map((e, index) => (
+            <span key={index}>{e}<br/></span>
+          ))}
+          />
               </div>
             </div>
                         
