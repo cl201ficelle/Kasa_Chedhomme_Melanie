8e70b4bfa9445acd97680e65f275d1094636ec2a
Melanie C <melanie.chedhomme96@gmail.com>
2023-11-09 11:46:37 +0100

    collapse texte transition ok

--- my-app/src/components/Collapse.js
+++ my-app/src/components/Collapse.js
@@ -1,32 +1,31 @@
-import React from 'react';
-import { useEffect, useState } from "react"
+import React, { useState, useRef } from 'react';
 
+const Collapsible = (props) => {
+  const [open, setOpen] = useState(false);
+  const contentRef = useRef();
 
-
-const Collapsible =(props)=>{
-    const [open, setOpen] = useState(false);
-    const toggle = () => {
+  const toggle = () => {
     setOpen(!open);
-
   };
-    return(
-        
-        <div className='button_text_collapse'>
-         <button onClick={toggle} className='button_collapse'>
-  {props.label}
-  <i className={`fa-solid fa-chevron-${open ? 'down' : 'up'} arrow`} id="rotateIcon"></i>
-</button>
-        {open && (
-          <div className="text_collapse">
-            <p>{props.description}</p>
-          </div>
-        )}
-      
-      </div>    
-    )
+
+  return (
+    <div className='button_text_collapse'>
+      <button onClick={toggle} className='button_collapse'>
+        {props.label}
+        <i className={`fa-solid fa-chevron-${open ? 'down' : 'up'} arrow`} id="rotateIcon"></i>
+      </button>
+      <div className="content-parent" ref={contentRef} style={open ? { height: contentRef.current.scrollHeight + "px" } : { height: "0px" }}>
+        <div className="collapse_content">{props.description}</div>
+      </div>
+    </div>
+  );
 }
 
 export default Collapsible;
+
+
+
+
 
 const icon = document.getElementById('rotateIcon');
 let isRotated = false;
