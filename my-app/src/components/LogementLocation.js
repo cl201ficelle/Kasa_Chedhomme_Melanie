b3ae1a3f0723d53ff88fe59a0a07414a287a1767
Melanie C <melanie.chedhomme96@gmail.com>
2023-11-17 12:24:51 +0100

    location utilisation const

--- my-app/src/components/LogementLocation.js
+++ my-app/src/components/LogementLocation.js
@@ -3,7 +3,7 @@ import "../styles/components/LogementLocation.scss";
 const LocationLogement = (props) => {
   const location = props.location;
     return (
-      <h3 className="logement_location">{props.location}</h3>
+      <h3 className="logement_location">{location}</h3>
     );
 };
 
